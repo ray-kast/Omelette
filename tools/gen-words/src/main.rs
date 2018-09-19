90afccaa7a148708bffe12280f04dd5e751bf5d5
rookie1024 <rookie1286@gmail.com>
2018-09-19 01:21:36 -0700

    Made gen-words multithreaded.

--- tools/gen-words/src/main.rs
+++ tools/gen-words/src/main.rs
@@ -7,17 +7,24 @@ extern crate lazy_static;
 #[macro_use]
 extern crate serde_derive;
 
+mod thread_pool;
+
 use regex::Regex;
 use std::{
   collections::{HashMap, HashSet},
   fs::File,
-  io,
-  io::{prelude::*, BufReader, BufWriter},
+  io::{self, prelude::*, BufReader, BufWriter},
   str,
-  time::Instant,
+  sync::{
+    atomic::{AtomicUsize, Ordering},
+    mpsc::{channel, Sender},
+    Arc,
+  },
+  time::Instant
 };
+use thread_pool::ThreadPool;
 
-#[derive(Serialize)]
+#[derive(Clone, Serialize)]
 struct WordlistForm(String, String); // (blanked, full)
 
 #[derive(Serialize)]
@@ -53,11 +60,30 @@ fn is_subseq(count: &CharCounts, of: &CharCounts) -> bool {
   count.iter().all(|(c, n)| n <= of.get(c).unwrap_or(&0))
 }
 
-fn main() {
-  let min_valid_len = 3;
-  let min_len = 4;
-  let max_len = 8;
+static MIN_VALID_LEN: usize = 3;
+static MIN_LEN: usize = 4;
+static MAX_LEN: usize = 8;
 
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct Normalized(String); // Used as a string with nonword character stripped
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct Depermuted(String); // Used as a Normalized with its characters sorted
+
+struct Stage1 {
+  permutations: HashMap<Depermuted, HashSet<Normalized>>,
+  counts: HashMap<Depermuted, CharCounts>,
+  valid_subwords: HashSet<Depermuted>,
+  len_groups: HashMap<usize, HashSet<Depermuted>>,
+  forms: HashMap<Normalized, Vec<WordlistForm>>,
+}
+
+struct Stage2<'a> {
+  sets: HashMap<Depermuted, Vec<Normalized>>, // TODO: can I go back to borrowing inside the vec?
+  set_keys: HashMap<usize, Vec<&'a Depermuted>>,
+  used_words: HashSet<Normalized>,
+}
+
+fn stage_1() -> Stage1 {
   let mut words = Vec::new();
 
   {
@@ -73,21 +99,13 @@ fn main() {
 
   println!("read {} word(s)", words.len());
 
-  #[derive(Clone, PartialEq, Eq, Hash)]
-  struct Normalized(String); // Used as a string with nonword character stripped
-  #[derive(Clone, PartialEq, Eq, Hash)]
-  struct Depermuted(String); // Used as a Normalized with its characters sorted
-
   let mut permutations: HashMap<Depermuted, HashSet<Normalized>> =
     HashMap::new();
   let mut counts: HashMap<Depermuted, CharCounts> = HashMap::new();
   let mut len_groups: HashMap<usize, HashSet<Depermuted>> = HashMap::new();
   let mut valid_subwords: HashSet<Depermuted> = HashSet::new();
-  let mut used_words: HashSet<Normalized> = HashSet::new();
 
   let mut forms: HashMap<Normalized, Vec<WordlistForm>> = HashMap::new();
-  let mut sets: HashMap<Depermuted, Vec<&Normalized>> = HashMap::new();
-  let mut set_keys: HashMap<usize, Vec<&Depermuted>> = HashMap::new();
 
   lazy_static! {
     static ref REJECT_RE: Regex = Regex::new(r"[\d\s]").unwrap();
@@ -133,7 +151,7 @@ fn main() {
           }
         }
 
-        if depermuted.0.len() >= min_valid_len {
+        if depermuted.0.len() >= MIN_VALID_LEN {
           valid_subwords.insert(depermuted);
         }
       }
@@ -147,56 +165,96 @@ fn main() {
   println!("{} depermuted", permutations.len());
   println!("{} valid subword(s)", valid_subwords.len());
 
-  for len in min_len..max_len + 1 {
-    println!("processing {}-letter words...", len);
+  Stage1 {
+    permutations,
+    counts,
+    valid_subwords,
+    len_groups,
+    forms,
+  }
+}
 
-    let start = Instant::now();
+fn stage_2<'a>(s1: &'a Arc<Stage1>) -> Stage2<'a> {
+  let mut sets: HashMap<Depermuted, Vec<Normalized>> = HashMap::new(); // TODO: can I go back to borrowing inside the vec?
+  let mut set_keys: HashMap<usize, Vec<&Depermuted>> = HashMap::new();
 
-    let mut keys: Vec<&Depermuted> = Vec::new();
+  let mut used_words: HashSet<Normalized> = HashSet::new();
 
-    let iter = counts.iter().filter(|(d, _)| d.0.len() == len);
+  let (set_tx, set_rx) = channel();
 
-    let total = iter.clone().count();
+  let done = Arc::new(AtomicUsize::new(0));
 
-    for (i, (depermuted, count)) in iter.enumerate() {
-      if i % 10 == 0 {
-        print!("\r\x1b[2K({}/{}) {}", &i, &total, &depermuted.0);
-        io::stdout().flush().unwrap();
-      }
+  {
+    let worker: ThreadPool<_> = ThreadPool::new(
+      (0..10)
+        .map(|_| (Arc::clone(s1), done.clone(), set_tx.clone()))
+        .collect(),
+      |_id,
+       (s1, done, set_tx): &(Arc<Stage1>, Arc<AtomicUsize>, Sender<_>),
+       (depermuted, count): (Depermuted, CharCounts)| {
+        let i = done.fetch_add(1, Ordering::Relaxed);
+        if i % 10 == 0 {
+          print!("\r\x1b[2K({}) {}", i, &depermuted.0);
+          io::stdout().flush().unwrap();
+        }
 
-      let mut list: Vec<_> = valid_subwords
-        .iter()
-        .filter(|deperm2| {
-          deperm2.0.len() <= depermuted.0.len()
-            && is_subseq(&counts[*deperm2], &count)
-        })
-        .flat_map(|d| &permutations[d])
-        .collect();
+        let mut list: Vec<_> = s1
+          .valid_subwords
+          .iter()
+          .filter(|deperm2| {
+            deperm2.0.len() <= depermuted.0.len()
+              && is_subseq(&s1.counts[*deperm2], &count)
+          })
+          .flat_map(|d| s1.permutations[d].clone()) // TODO: can I go back to borrowing this?
+          .collect();
 
-      list.sort_by(|a, b| a.0.len().cmp(&b.0.len()).then(a.0.cmp(&b.0)));
+        list.sort_by(|a, b| a.0.len().cmp(&b.0.len()).then(a.0.cmp(&b.0)));
 
-      for norm in &list {
-        used_words.insert(Normalized::clone(norm));
-      }
-
-      sets.insert(depermuted.clone(), list);
-      keys.push(depermuted);
-    }
-
-    let end = Instant::now();
-    let time = end - start;
-
-    println!(
-      "\r\x1b[2K{} processed in {}.{:02}s",
-      total,
-      time.as_secs(),
-      time.subsec_millis() / 10
+        set_tx
+          .send((depermuted, list))
+          .expect("failed to send result");
+      },
     );
 
-    set_keys.insert(len, keys);
+    for len in MIN_LEN..MAX_LEN + 1 {
+      let mut keys: Vec<&Depermuted> = Vec::new();
+
+      for (_, depermuted) in s1.len_groups[&len].iter().enumerate() {
+        worker.queue((depermuted.clone(), s1.counts[depermuted].clone()));
+        keys.push(depermuted);
+      }
+
+      set_keys.insert(len, keys);
+    }
+
+    worker.join();
+
+    println!();
   }
 
-  forms.retain(|k, _| used_words.contains(k));
+  for (depermuted, list) in set_rx.try_iter() {
+    for norm in &list {
+      used_words.insert(Normalized::clone(norm));
+    }
+
+    sets.insert(depermuted, list);
+  }
+
+  Stage2 {
+    sets,
+    set_keys,
+    used_words,
+  }
+}
+
+fn main() {
+  let s1 = Arc::new(stage_1());
+
+  let s2 = stage_2(&s1);
+
+  let mut forms = s1.forms.clone();
+
+  forms.retain(|k, _| s2.used_words.contains(k));
 
   let list = {
     let form_vec: Vec<(Normalized, Vec<WordlistForm>)>;
@@ -212,9 +270,10 @@ fn main() {
         .map(|(i, (n, _))| (n, i))
         .collect();
 
-      set_vec = sets
+      set_vec = s2
+        .sets
         .into_iter()
-        .map(|(d, n)| (d, n.into_iter().map(|n| forms[n]).collect()))
+        .map(|(d, n)| (d, n.into_iter().map(|n| forms[&n]).collect()))
         .collect();
 
       let sets: HashMap<_, _> = set_vec
@@ -223,7 +282,8 @@ fn main() {
         .map(|(i, (d, _))| (d, i))
         .collect();
 
-      set_key_map = set_keys
+      set_key_map = s2
+        .set_keys
         .into_iter()
         .map(|(l, d)| (l, d.into_iter().map(|d| sets[d]).collect()))
         .collect();
---
+++ tools/gen-words/src/thread_pool.rs
@@ -0,0 +1,140 @@
+use std::{
+  collections::HashSet,
+  sync::{
+    mpsc::{channel, Receiver, Sender},
+    Arc, Mutex,
+  },
+  thread::{self, JoinHandle},
+};
+
+pub struct ThreadPool<T>
+where
+  T: Send + 'static,
+{
+  task_tx: Sender<T>,
+  sched_tx: Sender<SchedMessage>,
+  sched_handle: JoinHandle<()>,
+}
+
+enum SchedMessage {
+  Sleep(usize),
+  WakeAll,
+  Stop,
+}
+
+enum WorkerMessage {
+  Continue,
+  Stop,
+}
+
+use self::{SchedMessage as SM, WorkerMessage as WM};
+
+// TODO: is the 'static necessary, or can it be parameterized?
+impl<T> ThreadPool<T>
+where
+  T: Send + 'static,
+{
+  pub fn new<C, F>(closures: Vec<C>, f: F) -> Self
+  where
+    C: Send + 'static,
+    F: Fn(usize, &C, T) -> () + Clone + Send + 'static,
+  {
+    let (task_tx, task_rx) = channel();
+
+    let task_rx = Arc::new(Mutex::new(task_rx));
+
+    let (sched_tx, sched_rx) = channel();
+
+    let workers: Vec<_> = closures
+      .into_iter()
+      .enumerate()
+      .map(|(id, closure)| {
+        let (tx, rx) = channel::<WorkerMessage>();
+
+        let task_rx = task_rx.clone();
+        let sched_tx = sched_tx.clone();
+        let f = f.clone();
+
+        let handle = thread::spawn(move || loop {
+          match rx.recv().expect("failed to receive WM") {
+            WM::Continue => loop {
+              match {
+                let task_rx = task_rx.lock().expect("failed to receive task");
+                task_rx.try_recv()
+              } {
+                Ok(t) => f(id, &closure, t),
+                Err(_) => break,
+              }
+            },
+            WM::Stop => break,
+          }
+
+          sched_tx
+            .send(SM::Sleep(id))
+            .expect("failed to send SM::Sleep");
+        });
+
+        (handle, tx)
+      })
+      .collect();
+
+    let sched_handle = thread::spawn(move || {
+      let mut awake: HashSet<usize> = HashSet::new();
+      let mut stopping = false;
+
+      loop {
+        match sched_rx.recv().expect("failed to receive SM") {
+          SM::Sleep(i) => {
+            awake.remove(&i);
+
+            if awake.len() == 0 && stopping {
+              for (_, tx) in &workers {
+                tx.send(WM::Stop).expect("failed to send WM::Stop");
+              }
+
+              for (handle, _) in workers {
+                handle.join().expect("a worker died");
+              }
+
+              break;
+            }
+          }
+          SM::WakeAll => {
+            for i in 0..workers.len() {
+              if awake.insert(i) {
+                workers[i]
+                  .1
+                  .send(WM::Continue)
+                  .expect("failed to send WM::Continue");
+              }
+            }
+          }
+          SM::Stop => stopping = true,
+        }
+      }
+    });
+
+    Self {
+      task_tx,
+      sched_tx,
+      sched_handle,
+    }
+  }
+
+  // TODO: make this return a Result maybe?
+  pub fn queue(&self, task: T) {
+    self.task_tx.send(task).expect("failed to send task");
+    self
+      .sched_tx
+      .send(SM::WakeAll)
+      .expect("failed to send SM::WakeAll");
+  }
+
+  pub fn join(self) {
+    self
+      .sched_tx
+      .send(SM::Stop)
+      .expect("failed to send SM::Stop");
+    self.sched_handle.join().expect("scheduler died");
+  }
+}
